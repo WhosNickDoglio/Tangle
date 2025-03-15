9353486c8094cd933d2221589071bda4cd5a3d6e
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-03-15 17:39:35 -0400

    Add JVM args for tests that are necessary when running on JDKs above 16

--- buildSrc/src/main/kotlin/dev/whosnickdoglio/convention/tangle/common.kt
+++ buildSrc/src/main/kotlin/dev/whosnickdoglio/convention/tangle/common.kt
@@ -51,6 +51,18 @@ fun Project.common() {
     }
   tasks.withType<Test> {
     useJUnitPlatform()
+    jvmArgs(
+      "--add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
+      "--add-opens=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
+      "--add-opens=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED",
+      "--add-opens=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
+      "--add-opens=jdk.compiler/com.sun.tools.javac.jvm=ALL-UNNAMED",
+      "--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
+      "--add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED",
+      "--add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED",
+      "--add-opens=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
+      "--add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
+    )
 
     testLogging {
       events = setOf(PASSED, FAILED)
